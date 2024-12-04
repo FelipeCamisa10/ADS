9a8d7684d0072dbecc7757db703d0c7b01501591
Felipe Gabriel <83315839+FelipeCamisa10@users.noreply.github.com>
2024-12-04 10:42:54 -0300

    Add files via upload

---
+++ RLF/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <p>
+        Felipe Gabriel
+    </p>
+    <p>
+        <a href="https://github.com/FelipeCamisa10"> GIT HUB </a>
+    </p>
+    <p>
+        <a href="hhttps://www.linkedin.com/in/felipejpeg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> LINKEDIN </a>
+    </p>
+    <p>
+        <a href="https://www.instagram.com/felipejpeg"> INSTAGRAM </a>
+    </p>
+    <p>
+     
+    </p>
+</footer>
